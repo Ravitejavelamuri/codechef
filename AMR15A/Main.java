4729a7a3b164e18cebee131f5fdbd505e4cc7c57
Ravitejavelamuri <39962669+Ravitejavelamuri@users.noreply.github.com>
2020-09-04 23:06:52 +0530

    Update Main.java

--- AMR15A/Main.java
+++ AMR15A/Main.java
@@ -7,15 +7,16 @@ public class Main {
 
 		int N = sc.nextInt();
 		int[] A = new int[N];
-		for (int i = 0; i < A.length; i++) {
+		for (int i = 0; i < N; i++) {
 			A[i] = sc.nextInt();
 		}
 		System.out.println(solve(A) ? "READY FOR BATTLE" : "NOT READY");
 
-		sc.close();
 	}
 
 	static boolean solve(int[] A) {
-		return Arrays.stream(A).filter(x -> x % 2 == 0).count() > Arrays.stream(A).filter(x -> x % 2 != 0).count();
+		int k=0;
+		k = Arrays.stream(A).filter(x -> x % 2 == 0).count() > Arrays.stream(A).filter(x -> x % 2 != 0).count();
+		return k;
 	}
 }
